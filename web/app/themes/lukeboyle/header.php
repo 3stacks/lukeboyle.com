ab048f5a0a4974573e03b6ee4740de9098a30664
Luke Boyle <luke.boyle.au@gmail.com>
2016-09-12 23:33:43 +1000

    feat: add the structure for the heading

--- web/app/themes/lukeboyle/header.php
+++ web/app/themes/lukeboyle/header.php
@@ -1,40 +1,34 @@
-<?php
-/**
- * The template for displaying the header
- *
- * Displays all of the head element and everything up until the "site-content" div.
- *
- * @package WordPress
- * @subpackage Twenty_Fifteen
- * @since Twenty Fifteen 1.0
- */
-?><!DOCTYPE html>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js">
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width">
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-    <!--[if lt IE 9]>
-    <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
-    <![endif]-->
+    <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/vendor.js" defer></script>
+    <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/bundle.js" defer></script>
+    <link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/style.css">
     <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
     <a class="skip-link screen-reader-text" href="#main">Skip to content</a>
     <header class="site-header">
-        <div class="site-branding">
-            <?php
+        <div class="max-width-container">
+            <div class="site-nav">
+                <?php
 
-            if ( is_front_page() && is_home() ) : ?>
-                <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-            <?php else : ?>
-                <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-            <?php endif;
-
-            ?>
-            <button class="secondary-toggle"><?php _e( 'Menu and widgets', 'twentyfifteen' ); ?></button>
+                if ( is_front_page() && is_home() ) : ?>
+                    <h1 class="site-nav--logo"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+                <?php else : ?>
+                    <p class="site-nav--logo"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+                <?php endif;
+                wp_nav_menu( array(
+                    'theme_location' => 'primary-menu',
+                    'container' => false,
+                    'menu_id' => 'menu'
+                ));
+                ?>
+            </div>
         </div>
     </header>
-    <div id="content" class="site-content">
