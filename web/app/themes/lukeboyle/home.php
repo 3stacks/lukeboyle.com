f7d20a403086874a0462206547570ddbaf2886a9
Luke Boyle <luke.boyle.au@gmail.com>
2016-09-14 09:03:18 +1000

    chore: finish the static markup for a post

--- web/app/themes/lukeboyle/home.php
+++ web/app/themes/lukeboyle/home.php
@@ -30,11 +30,28 @@
                     get_template_part( 'partials/content', get_post_format() );
                     ?>
                     <article class="blog-post">
-                        <h1 class="blog-post--title">
-                            Title
-                        </h1>
-                        <div class="blog-post--meta">
-                            <time></time>
+                        <header>
+                            <h1 class="blog-post--title">
+                                <a href="#">
+                                    Title
+                                </a>
+                            </h1>
+                            <div class="blog-post--meta">
+                                <time datetime=""></time>
+                                <ul class="blog-post--meta--categories">
+                                    <li>
+                                        Web Dev //static
+                                    </li>
+                                </ul>
+                                <ul class="blog-post--meta--tags">
+                                    <li>
+                                        React //static
+                                    </li>
+                                </ul>
+                            </div>
+                        </header>
+                        <div class="blog-post--content">
+
                         </div>
                     </article>
                     <?php
---
+++ web/app/themes/lukeboyle/single-post.php
@@ -0,0 +1,25 @@
+<article class="blog-post">
+    <header>
+        <h1 class="blog-post--title">
+            <a href="#">
+                Title
+            </a>
+        </h1>
+        <div class="blog-post--meta">
+            <time datetime=""></time>
+            <ul class="blog-post--meta--categories">
+                <li>
+                    Web Dev //static
+                </li>
+            </ul>
+            <ul class="blog-post--meta--tags">
+                <li>
+                    React //static
+                </li>
+            </ul>
+        </div>
+    </header>
+    <div class="blog-post--content">
+
+    </div>
+</article>
