8ecfcf1d7e1e0e8f7d1378277a1894c4dc726f89
Luke Boyle <luke.boyle.au@gmail.com>
2016-09-14 08:53:24 +1000

    chore: roughly template the blog page

--- web/app/themes/lukeboyle/home.php
+++ web/app/themes/lukeboyle/home.php
@@ -1,13 +1,43 @@
 <?php /* Template Name: Blog */ ?>
 <?php get_header(); ?>
-
+    <div class="blog-header">
+        <h1 class="blog-header--site-name">
+           Boyleing Point //Static//
+        </h1>
+        <p>
+            7/11 was an inside job //static//
+        </p>
+        <div class="blog-header--menu">
+            // extra menu //
+            <ul>
+                <li>
+                    Home
+                </li>
+                <li>
+                    Music
+                </li>
+                <li>
+                    Web Dev
+                </li>
+            </ul>
+        </div>
+    </div>
     <main id="main" class="site-main">
         <div class="max-width-container">
             <?php if ( have_posts() ) : ?>
                 <?php
                 while ( have_posts() ) : the_post();
                     get_template_part( 'partials/content', get_post_format() );
-                    // End the loop.
+                    ?>
+                    <article class="blog-post">
+                        <h1 class="blog-post--title">
+                            Title
+                        </h1>
+                        <div class="blog-post--meta">
+                            <time></time>
+                        </div>
+                    </article>
+                    <?php
                 endwhile;
 
             else :
