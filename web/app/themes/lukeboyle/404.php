cbb5e2a91049f9e198e1c0ce76043e635f24ef06
Luke Boyle <luke.boyle.au@gmail.com>
2016-09-14 18:44:48 +1000

    feat: add 404 page

---
+++ web/app/themes/lukeboyle/404.php
@@ -0,0 +1,16 @@
+<?php /* Template Name: Blog */ ?>
+<?php get_header(); ?>
+    <main class="not-found-error">
+        <h1 class="not-found-error--title">
+            404 - There's nothing here
+        </h1>
+        <p class="not-found-error--terrible-joke">
+            Back from whence you came!
+        </p>
+        <p class="not-found-error--links">
+            <a href="/">
+                Home
+            </a>
+        </p>
+    </main>
+<?php get_footer(); ?>
