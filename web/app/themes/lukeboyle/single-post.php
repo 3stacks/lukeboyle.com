4655c24ebc54c95b8413ba746db07e4b73695c18
Luke Boyle <luke.boyle.au@gmail.com>
2016-09-15 18:54:41 +1000

    feat: output the real categories and tags

--- web/app/themes/lukeboyle/single-post.php
+++ web/app/themes/lukeboyle/single-post.php
@@ -1,29 +1,45 @@
 <?php get_header(); ?>
-
-<?php while ( have_posts() ) : the_post(); ?>
-    <article class="blog-post">
-        <header>
-            <h1 class="blog-post--title">
-                <?php echo the_title(); ?>
-            </h1>
-            <div class="blog-post--meta">
-                <time datetime=""></time>
-                <ul class="blog-post--meta--categories">
-                    <li>
-                        Web Dev //static
-                    </li>
-                </ul>
-                <ul class="blog-post--meta--tags">
-                    <li>
-                        React //static
-                    </li>
-                </ul>
-            </div>
-        </header>
-        <div class="blog-post--content">
-            <?php the_content() ?>
+    <div class="blog-header">
+        <h1 class="blog-header--site-name">
+            <?php bloginfo('name'); ?>
+        </h1>
+        <p class="blog-header--description">
+            <?php bloginfo('description'); ?>
+        </p>
+        <ul class="blog-header--menu">
+            <?php wp_list_categories(array('title_li' => '')) ?>
+        </ul>
+    </div>
+    <main id="main" class="site-main">
+        <div class="max-width-container blog">
+            <?php while ( have_posts() ) : the_post(); ?>
+                <article class="blog-post">
+                    <header>
+                        <h1 class="blog-post--title">
+                            <?php echo the_title(); ?>
+                        </h1>
+                        <div class="blog-post--meta">
+                            <time datetime="<?php echo $post->post_date ?>">
+                                <?php echo $post->post_date ?>
+                            </time>
+                            <ul class="blog-post--meta--categories">
+                                <li>
+                                    <?php wp_get_post_categories($post->ID); ?>
+                                </li>
+                            </ul>
+                            <ul class="blog-post--meta--tags">
+                                <li>
+                                    React //static
+                                </li>
+                            </ul>
+                        </div>
+                    </header>
+                    <div class="blog-post--content">
+                        <?php the_content() ?>
+                    </div>
+                </article>
+            <?php endwhile; ?>
         </div>
-    </article>
-<?php endwhile; ?>
+    </main>
 
 <?php get_footer(); ?>
