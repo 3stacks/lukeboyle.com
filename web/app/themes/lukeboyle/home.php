fa29310d94e8796cd45cc360fc7b22a9acb7fb1e
Luke Boyle <luke.boyle.au@gmail.com>
2016-09-15 20:39:37 +1000

    refactor: add final home blog markup

--- web/app/themes/lukeboyle/home.php
+++ web/app/themes/lukeboyle/home.php
@@ -19,22 +19,35 @@
                             <?php echo the_title(); ?>
                         </h1>
                         <div class="blog-post--meta">
-                            <time datetime=""><?php echo $post->post_date ?></time>
+                            <time datetime="<?php echo $post->post_date ?>">
+                                <?php echo date('d F Y', strtotime($post->post_date)); ?>
+                            </time>
                             <ul class="blog-post--meta--categories">
-                                <li>
-                                    <?php var_dump(wp_get_post_categories($post->ID)); ?>
-                                </li>
-                            </ul>
-                            <ul class="blog-post--meta--tags">
-                                <li>
-                                    React //static
-                                </li>
+                                <?php $categories = wp_get_post_categories($post->ID); ?>
+                                <?php foreach ($categories as $category) { ?>
+                                    <li>
+                                        <a href="<?php echo get_category_link($category) ?>">
+                                            <?php echo get_the_category_by_ID($category); ?>
+                                        </a>
+                                    </li>
+                                <?php } ?>
                             </ul>
                         </div>
                     </header>
                     <div class="blog-post--content">
                         <?php the_content() ?>
                     </div>
+                    <ul class="blog-post--tags">
+                        <?php $tags = wp_get_post_tags($post->ID); ?>
+                        <?php if (!!$tags) { echo 'Filed under:'; } ?>
+                        <?php foreach ($tags as $tag) { ?>
+                            <li>
+                                <a href="<?php echo get_tag_link($tag->term_id) ?>">
+                                    <?php echo $tag->name ?>
+                                </a>
+                            </li>
+                        <?php } ?>
+                    </ul>
                 </article>
             <?php endwhile; ?>
         </div>
