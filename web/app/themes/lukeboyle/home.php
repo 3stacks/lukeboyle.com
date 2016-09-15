9bb90b2a78c1462ad591c66b63abcae6cf7fc507
Luke Boyle <luke.boyle.au@gmail.com>
2016-09-16 00:40:11 +1000

    feat: add pagination on blog

--- web/app/themes/lukeboyle/home.php
+++ web/app/themes/lukeboyle/home.php
@@ -12,44 +12,54 @@
     </div>
     <main id="main" class="site-main">
         <div class="max-width-container blog">
-            <?php while ( have_posts() ) : the_post(); ?>
-                <article class="blog-post">
-                    <header>
-                        <h1 class="blog-post--title">
-                            <?php echo the_title(); ?>
-                        </h1>
-                        <div class="blog-post--meta">
-                            <time datetime="<?php echo $post->post_date ?>">
-                                <?php echo date('d F Y', strtotime($post->post_date)); ?>
-                            </time>
-                            <ul class="blog-post--meta--categories">
-                                <?php $categories = wp_get_post_categories($post->ID); ?>
-                                <?php foreach ($categories as $category) { ?>
-                                    <li>
-                                        <a href="<?php echo get_category_link($category) ?>">
-                                            <?php echo get_the_category_by_ID($category); ?>
-                                        </a>
-                                    </li>
-                                <?php } ?>
-                            </ul>
+            <?php if (have_posts()) : ?>
+                <?php while ( have_posts() ) : the_post(); ?>
+                    <article class="blog-post">
+                        <header>
+                            <h1 class="blog-post--title">
+                                <?php echo the_title(); ?>
+                            </h1>
+                            <div class="blog-post--meta">
+                                <time datetime="<?php echo $post->post_date ?>">
+                                    <?php echo date('d F Y', strtotime($post->post_date)); ?>
+                                </time>
+                                <ul class="blog-post--meta--categories">
+                                    <?php $categories = wp_get_post_categories($post->ID); ?>
+                                    <?php foreach ($categories as $category) { ?>
+                                        <li>
+                                            <a href="<?php echo get_category_link($category) ?>">
+                                                <?php echo get_the_category_by_ID($category); ?>
+                                            </a>
+                                        </li>
+                                    <?php } ?>
+                                </ul>
+                            </div>
+                        </header>
+                        <div class="blog-post--content">
+                            <?php the_content() ?>
                         </div>
-                    </header>
-                    <div class="blog-post--content">
-                        <?php the_content() ?>
-                    </div>
-                    <ul class="blog-post--tags">
-                        <?php $tags = wp_get_post_tags($post->ID); ?>
-                        <?php if (!!$tags) { echo 'Filed under:'; } ?>
-                        <?php foreach ($tags as $tag) { ?>
-                            <li>
-                                <a href="<?php echo get_tag_link($tag->term_id) ?>">
-                                    <?php echo $tag->name ?>
-                                </a>
-                            </li>
-                        <?php } ?>
-                    </ul>
-                </article>
-            <?php endwhile; ?>
+                        <ul class="blog-post--tags">
+                            <?php $tags = wp_get_post_tags($post->ID); ?>
+                            <?php if (!!$tags) { echo 'Filed under:'; } ?>
+                            <?php foreach ($tags as $tag) { ?>
+                                <li>
+                                    <a href="<?php echo get_tag_link($tag->term_id) ?>">
+                                        <?php echo $tag->name ?>
+                                    </a>
+                                </li>
+                            <?php } ?>
+                        </ul>
+                    </article>
+                <?php endwhile; ?>
+                <div class="pagination">
+                    <div class="pagination--previous"><?php next_posts_link('Older'); ?></div>
+                    <div class="pagination--next"><?php previous_posts_link('Newer'); ?></div>
+                </div>
+            <?php else : ?>
+                <p>
+                    Sorry, no posts matched your criteria.
+                </p>
+            <?php endif; ?>
         </div>
     </main>
 
