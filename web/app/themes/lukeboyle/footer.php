41ed71ef42607b78b6ac82cb99a7837a58f7b913
Luke Boyle <luke.boyle.au@gmail.com>
2016-09-15 20:39:11 +1000

    chore: add footer markup

--- web/app/themes/lukeboyle/footer.php
+++ web/app/themes/lukeboyle/footer.php
@@ -1,7 +1,15 @@
-<footer>
-
-</footer>
-<?php wp_footer(); ?>
-
-</body>
+        <footer class="footer">
+            <div class="footer--inner">
+                <p class="footer--inner--copyright">
+                    &copy; Luke Boyle <?php echo date('Y'); ?>
+                </p>
+                <p class="footer--inner--twitter">
+                    <a href="https://twitter.com/tricepidemic" title="Find me on Twitter">
+                        @tricepidemic
+                    </a>
+                </p>
+                <?php wp_footer(); ?>
+            </div>
+        </footer>
+    </body>
 </html>
