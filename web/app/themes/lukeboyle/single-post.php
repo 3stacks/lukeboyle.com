399c8721a0eb599ca96af5a1ac921652c49de53f
Luke Boyle <luke.boyle.au@gmail.com>
2016-09-16 08:48:37 +1000

    feat: Add single post apge

--- web/app/themes/lukeboyle/single-post.php
+++ web/app/themes/lukeboyle/single-post.php
@@ -12,40 +12,42 @@
     </div>
     <main id="main" class="site-main">
         <div class="max-width-container blog">
-            <?php while ( have_posts() ) : the_post(); ?>
-                <article class="blog-post">
-                    <header>
-                        <h1 class="blog-post--title">
-                            <?php echo the_title(); ?>
-                        </h1>
-                        <div class="blog-post--meta">
-                            <time datetime="<?php echo $post->post_date ?>">
-                                <?php echo $post->post_date ?>
-                            </time>
-                            <ul class="blog-post--meta--categories">
-                                <?php $categories = wp_get_post_categories($post->ID); ?>
-                                <?php foreach ($categories as $category) { ?>
-                                    <li>
-                                        <?php var_dump($category); ?>
+            <article class="blog-post">
+                <header>
+                    <h1 class="blog-post--title">
+                        <?php echo the_title(); ?>
+                    </h1>
+                    <div class="blog-post--meta">
+                        <time datetime="<?php echo $post->post_date ?>">
+                            <?php echo date('d F Y', strtotime($post->post_date)); ?>
+                        </time>
+                        <ul class="blog-post--meta--categories">
+                            <?php $categories = wp_get_post_categories($post->ID); ?>
+                            <?php foreach ($categories as $category) { ?>
+                                <li>
+                                    <a href="<?php echo get_category_link($category) ?>">
                                         <?php echo get_the_category_by_ID($category); ?>
-                                    </li>
-                                <?php } ?>
-                            </ul>
-                        </div>
-                    </header>
-                    <div class="blog-post--content">
-                        <?php the_content() ?>
+                                    </a>
+                                </li>
+                            <?php } ?>
+                        </ul>
                     </div>
-                    <ul class="blog-post--tags">
-                        <?php $tags = wp_get_post_tags($post->ID); ?>
-                        <?php foreach ($tags as $tag) { ?>
-                            <li>
+                </header>
+                <div class="blog-post--content">
+                    <?php the_content() ?>
+                </div>
+                <ul class="blog-post--tags">
+                    <?php $tags = wp_get_post_tags($post->ID); ?>
+                    <?php if (!!$tags) { echo 'Filed under:'; } ?>
+                    <?php foreach ($tags as $tag) { ?>
+                        <li>
+                            <a href="<?php echo get_tag_link($tag->term_id) ?>">
                                 <?php echo $tag->name ?>
-                            </li>
-                        <?php } ?>
-                    </ul>
-                </article>
-            <?php endwhile; ?>
+                            </a>
+                        </li>
+                    <?php } ?>
+                </ul>
+            </article>
         </div>
     </main>
 
