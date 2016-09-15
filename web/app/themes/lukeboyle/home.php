7f830620e4e5a9919b3fec51e3cb10ae394c210b
Luke Boyle <luke.boyle.au@gmail.com>
2016-09-15 13:39:26 +1000

    feat: markup the blog page

--- web/app/themes/lukeboyle/home.php
+++ web/app/themes/lukeboyle/home.php
@@ -1,29 +1,17 @@
-<?php /* Template Name: Blog */ ?>
 <?php get_header(); ?>
     <div class="blog-header">
         <h1 class="blog-header--site-name">
-           Boyleing Point //Static//
+           <?php bloginfo('name'); ?>
         </h1>
-        <p>
-            7/11 was an inside job //static//
+        <p class="blog-header--description">
+            <?php bloginfo('description'); ?>
         </p>
-        <div class="blog-header--menu">
-            // extra menu //
-            <ul>
-                <li>
-                    Home
-                </li>
-                <li>
-                    Music
-                </li>
-                <li>
-                    Web Dev
-                </li>
-            </ul>
-        </div>
+        <ul class="blog-header--menu">
+            <?php wp_list_categories(array('title_li' => '')) ?>
+        </ul>
     </div>
     <main id="main" class="site-main">
-        <div class="max-width-container">
+        <div class="max-width-container blog">
             <?php while ( have_posts() ) : the_post(); ?>
                 <article class="blog-post">
                     <header>
@@ -31,10 +19,10 @@
                             <?php echo the_title(); ?>
                         </h1>
                         <div class="blog-post--meta">
-                            <time datetime=""></time>
+                            <time datetime=""><?php echo $post->post_date ?></time>
                             <ul class="blog-post--meta--categories">
                                 <li>
-                                    Web Dev //static
+                                    <?php var_dump(wp_get_post_categories($post->ID)); ?>
                                 </li>
                             </ul>
                             <ul class="blog-post--meta--tags">
