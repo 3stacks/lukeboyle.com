28e17f871a03395d6e68c85dd1234c9a1412f845
Luke Boyle <luke.boyle.au@gmail.com>
2016-09-17 09:52:11 +1000

    feat: make the portfolio items a card

--- web/app/themes/lukeboyle/page-portfolio.php
+++ web/app/themes/lukeboyle/page-portfolio.php
@@ -5,13 +5,16 @@
         <?php $posts = get_posts(array('post_type' => 'portfolio_item')); ?>
         <div class="portfolio">
             <?php foreach ($posts as $post) { ?>
-                <div class="portfolio--item" style="background-image: url(<?php echo get_field('portfolio_image', $post->ID)['sizes']['medium'] ?>);">
-                    <div class="portfolio--item--inner">
-                        <h2 class="portfolio--item--title">
+                <div class="portfolio--item">
+                    <div class="portfolio--item--image">
+                        <img src="<?php echo get_field('portfolio_image', $post->ID)['sizes']['medium'] ?>" alt="<?php echo get_field('portfolio_image', $post->ID)['alt'] ?>">
+                    </div>
+                    <div class="portfolio--item--card">
+                        <h2 class="portfolio--item--card--title">
                             <?php echo $post->post_title ?>
                         </h2>
                         <?php echo get_field('portfolio_snippet', $post->ID);  ?>
-                        <a class="portfolio--item--link" href="<?php echo $post->guid ?>">
+                        <a class="portfolio--item--card--link button primary" href="<?php echo $post->guid ?>">
                             Read More
                         </a>
                     </div>
