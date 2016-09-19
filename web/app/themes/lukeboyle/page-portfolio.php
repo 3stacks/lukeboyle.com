1f403faa3eab1febc8a301039bf846d5354b986d
Luke Boyle <luke.boyle.au@gmail.com>
2016-09-19 19:39:47 +1000

    refactor: tidy php and convert guid to permalink

--- web/app/themes/lukeboyle/page-portfolio.php
+++ web/app/themes/lukeboyle/page-portfolio.php
@@ -5,18 +5,19 @@
         <?php $posts = get_posts(array('post_type' => 'portfolio_item')); ?>
         <div class="portfolio">
             <?php foreach ($posts as $post) { ?>
-                <div class="portfolio--item" data-prefetch="<?php echo $post->guid ?>">
+                <?php var_dump($post); ?>
+                <div class="portfolio--item" data-prefetch="<?php echo get_permalink($post->ID); ?>">
                     <div class="portfolio--item--image" style="background-image: url(<?php echo get_field('portfolio_image', $post->ID)['sizes']['medium'] ?>);">
                         <span class="visually-hidden">
-                            <?php echo get_field('portfolio_image', $post->ID)['alt'] ?>
+                            <?php echo get_field('portfolio_image', $post->ID)['alt']; ?>
                         </span>
                     </div>
                     <div class="portfolio--item--card">
                         <h2 class="portfolio--item--card--title">
-                            <?php echo $post->post_title ?>
+                            <?php echo $post->post_title; ?>
                         </h2>
                         <?php echo get_field('portfolio_snippet', $post->ID);  ?>
-                        <a class="portfolio--item--card--link button primary" href="<?php echo $post->guid ?>">
+                        <a class="portfolio--item--card--link button primary" href="<?php echo $post->guid; ?>">
                             Read More
                         </a>
                     </div>
