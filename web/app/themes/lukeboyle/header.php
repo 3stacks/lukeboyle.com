cb66d922b25c52ba864a975ebd816f4c245b9bcb
Luke Boyle <luke.boyle.au@gmail.com>
2016-09-24 12:37:26 +1000

    chore: add site description

--- web/app/themes/lukeboyle/header.php
+++ web/app/themes/lukeboyle/header.php
@@ -2,6 +2,7 @@
 <html <?php language_attributes(); ?> class="no-js">
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="description" content="Luke Boyle is a Front End Developer from Melbourne, Australia. Specialising in JavaScript web applications, he has experience with Angular, React and Vue Js.">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
