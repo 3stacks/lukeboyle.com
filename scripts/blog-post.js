7445d39d3a9f00fdd156792e0a26b928fd67c375
Luke Boyle <luke.boyle.au@gmail.com>
2017-10-01 13:09:40 +1100

    feat: add parsing markdown posts into jsx components

--- scripts/blog-post.js
+++ scripts/blog-post.js
@@ -1,54 +1,65 @@
 const glob = require('glob');
 const fs = require('fs');
+const path = require('path');
+const markdown = require('markdown');
+const camelCase = require('camel-case');
+const titleCase = require('title-case');
+const shell = require('shelljs');
 
-function generateComponent(contents) {
-	return `import React from "react";
-	import Helmet from "react-helmet";
-	
-	export default class Blog extends React.Component {
-		render() {
-			return (
-				<div className="max-width-container">
-					<Helmet>
-						<title>Blog | Luke Boyle</title>
-					</Helmet>
-					${contents}
-				</div>
-			);
-		}
-	}`;
+function getFileNameFromPath(path) {
+	const pathWithoutDirectory = path.includes('/') ? path.slice(path.indexOf('/') + 1, path.length - 1) : path;
+	const pathWithoutExtensions = pathWithoutDirectory.includes('.js') ? pathWithoutDirectory.slice(0, pathWithoutDirectory.indexOf('.js')) : pathWithoutDirectory;
+
+	if (pathWithoutDirectory.includes('/')) {
+		return getFileNameFromPath(pathWithoutExtensions);
+	} else {
+		return pathWithoutExtensions;
+	}
+}
+
+function generateComponent(post) {
+	const fileName = getFileNameFromPath(post.path);
+
+	return {
+		path: post.path,
+		fileName,
+		component: `
+			import React from 'react';
+			import Helmet from 'react-helmet';
+				
+			export default class ${camelCase(fileName)} extends React.Component {
+				render() {
+					return (
+						<div className="max-width-container">
+							<Helmet>
+								<title>${titleCase(fileName)} | Luke Boyle</title>
+							</Helmet>
+							${post.contents}
+						</div>
+					);
+				}
+			}
+		`
+	};
 }
 
 (() => {
-	glob('../blog-posts/**/*.md', {}, (err, files) => {
-		const posts = files.slice(Math.max(files.length - 5, 1));
+	glob('blog-posts/**/*.md', {}, (err, files) => {
+		const blogPosts = files.reduce((acc, curr) => {
+			acc.push({
+				path: curr,
+				contents: fs.readFileSync(curr, {encoding: 'utf-8'})
+			});
 
-		const content = posts.reduce((acc, curr) => {
-			const content = fs.readFileSync(curr, {encoding: 'utf-8'});
+			return acc;
+		}, []);
 
-			console.log(content.indexOf(''));
+		const components = blogPosts.map(generateComponent);
 
-			return content.slice(content.indexOf('#'), content.indexOf('\n'));
-
-			const something = acc + `
-<article class="blog-post">
-	<header>
-		<h2 class="blog-post--title">
-			${content.slice(content.indexOf('#'), content.indexOf('\n'))}
-		</h2>
-		<div class="blog-post--meta">
-			<time datetime="2017-04-26 15:50:17">
-				26 April 2017
-			</time>
-		</div>
-	</header>
-	<div class="blog-post--content">
-	
-	</div>
-</article>
-			`;
-		}, '');
-
-		console.log(content);
+		components.forEach(component => {
+			console.log(component.path);
+			shell.mkdir('-p', path.resolve(`${__dirname}/../src/${component.path.replace(`/${component.fileName}.md`, '')}`));
+			fs.writeFileSync(path.resolve(`${__dirname}/../src/${component.path.replace('.md', '.jsx')}`), component.component);
+		});
 	});
 })();
