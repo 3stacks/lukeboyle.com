1bdf69df62fa744ba1ee7a5b113916a3f1cb25b4
Luke Boyle <luke.boyle.au@gmail.com>
2017-10-01 13:49:27 +1100

    fix: Gatsby config

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,21 +4,41 @@ exports.modifyWebpackConfig = ({ config, stage }) => {
 
     switch (stage) {
         case 'develop':
+            config.loader('sass', {
+                test: /\.(sass|scss)$/,
+                exclude: /\.module\.(sass|scss)$/,
+                loaders: ['style', 'css', 'sass'],
+            });
+
+            break;
+
         case 'build-css':
+            config.loader('sass', {
+                test: /\.(sass|scss)$/,
+                exclude: /\.module\.(sass|scss)$/,
+                loader: ExtractTextPlugin.extract(['css?minimize', 'sass']),
+            });
+
+            break;
+
         case 'build-html':
+            config.loader('sass', {
+                test: /\.(sass|scss)$/,
+                exclude: /\.module\.(sass|scss)$/,
+                loader: 'null',
+            });
+
+            break;
+
         case 'build-javascript':
             config.loader('sass', {
                 test: /\.(sass|scss)$/,
                 exclude: /\.module\.(sass|scss)$/,
-                loader: null
-            });
-
-            config.loader('babel!react-markdown', {
-               test: /\.md$/
+                loader: 'null',
             });
 
             break;
     }
 
     return config
-};
+}
