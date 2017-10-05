4e2a6b31138b9fc0ff9408a8376d21b6ef46ea19
Luke Boyle <luke.boyle.au@gmail.com>
2017-10-05 22:32:24 +1100

    chore: fix urls

--- src/data/portfolio-items.js
+++ src/data/portfolio-items.js
@@ -11,49 +11,49 @@ export default [
     {
         name: 'Spectrum',
         snippet: 'Spectrum is a web-based editor for writing software specification documents.',
-        link: '/portfolio/spectrum',
+        link: '/portfolio/spectrum/',
         thumb: spectrumSrc
     },
     {
         name: 'Agander',
         snippet: 'A productivity app aggregating calendar and tasks. Reduce the noise in your daily agenda.',
-        link: '/portfolio/agander',
+        link: '/portfolio/agander/',
         thumb: aganderSrc
     },
     {
         name: 'Split Visualiser',
         snippet: 'A web-app for easily visualising and sharing the muscle engagement of your exercise routine.',
-        link: '/portfolio/split-visualiser',
+        link: '/portfolio/split-visualiser/',
         thumb: splitSrc
     },
     {
         name: 'Journey Tree',
         snippet: 'A multi-platform collection of social networking and business applications for childcare staff and parents.',
-        link: '/portfolio/journey-tree',
+        link: '/portfolio/journey-tree/',
         thumb: jtSrc
     },
     {
         name: 'Type With Apps',
         snippet: 'A web-app for easily visualising and sharing the muscle engagement of your exercise routine.',
-        link: '/portfolio/type-with-apps',
+        link: '/portfolio/type-with-apps/',
         thumb: typeSrc
     },
     {
         name: 'Broccoli and Co.',
         snippet: 'A case study about the scalability and organisation of SPAs made in Angular 1.5 with comprehensive testing.',
-        link: '/portfolio/broccoli-and-co',
+        link: '/portfolio/broccoli-and-co/',
         thumb: broccoliSrc
     },
     {
         name: 'Pypes',
         snippet: 'A micro-blogging platform with a RESTful interface and an Angular admin panel.',
-        link: '/portfolio/pypes',
+        link: '/portfolio/pypes/',
         thumb: pypesSrc
     },
     {
         name: 'Vices',
         snippet: 'A micro-application designed to tell you how much money you\'re wasting on your bad habit.',
-        link: '/portfolio/vices',
+        link: '/portfolio/vices/',
         thumb: vicesSrc
     }
 ];
