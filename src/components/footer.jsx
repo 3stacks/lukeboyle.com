61c662a3d9664ade3fbd6a3a0a88b5c0b76d92ee
Luke Boyle <luke.boyle.au@gmail.com>
2017-12-24 18:20:01 +1100

    chore: make markup more semantic in footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,14 +4,12 @@ export default function Footer({...otherProps}) {
     return (
         <footer className="footer">
             <div className="footer__inner max-width-container">
-                <p className="footer__inner__copyright">
+                <small className="footer__inner__copyright">
                     &copy; Luke Boyle 93' til infinity
-                </p>
-                <p className="footer__inner__twitter">
-                    <a href="https://www.linkedin.com/in/luke-boyle">
-                        Luke Boyle on LinkedIn
-                    </a>
-                </p>
+                </small>
+                <a href="https://www.linkedin.com/in/luke-boyle">
+                    Luke Boyle on LinkedIn
+                </a>
             </div>
         </footer>
     );
