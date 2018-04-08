97d4e1155146644d8e5f2b61b476646d54cfaeb1
Luke Boyle <luke.boyle.au@gmail.com>
2018-04-08 15:25:07 +1000

    chore: add constants

---
+++ src/constants/index.js
@@ -0,0 +1,16 @@
+export const MY_NAME = 'Luke Boyle';
+export const META_DESCRIPTION = {
+	HOME: 'Luke Boyle is a Front End Developer from Melbourne, Australia. Specialising in JavaScript web applications, he has experience with Angular, React and Vue Js.'
+};
+export const PORTFOLIO_ITEM_NAMES = {
+	THIS_WEBSITE: 'This website',
+	GUESSTIMATE: 'Guesstimate',
+	SPECTRUM: 'Spectrum',
+	AGANDER: 'Agander',
+	SPLIT_VISUALISER: 'Split Visualiser',
+	JOURNEY_TREE: 'Journey Tree',
+	TYPE_WITH_APPS: 'Type With Apps',
+	BROCCOLI: 'Broccoli and Co.',
+	PYPES: 'Pypes',
+	VICES: 'Vices',
+};
