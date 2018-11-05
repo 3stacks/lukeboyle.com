3faccdc34fe5eaa70e05e79209fbe22c0328f7d3
Luke Boyle <luke.boyle.au@gmail.com>
2018-11-06 01:30:19 +1100

    chore: remove google analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,12 +5,6 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
-    {
-      resolve: 'gatsby-plugin-google-analytics',
-      options: {
-        trackingId: 'UA-84326776-1',
-      },
-    },
     'gatsby-plugin-offline'
   ]
 };
