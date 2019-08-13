bd0825f0e62b3e58c14f9ee44e163f863e7a212f
Luke Boyle <me@lukeboyle.com>
2019-08-13 19:29:43 +1000

    chore: update workflow

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -10,7 +10,7 @@ AWS.config = new AWS.Config({
 		accessKeyId: process.env.AWS_ACCESS_KEY_ID,
 		secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 	}),
-	region: 'ap-southeast-2'
+	region: process.env.AWS_DEFAULT_REGION
 });
 
 glob('./public/**/*', {}, (err, files) => {
