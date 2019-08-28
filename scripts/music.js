a27255c3ada0bcd2a8504033a157eed4b4429f39
Luke Boyle <me@lukeboyle.com>
2019-08-28 20:13:36 +1000

    chore: add try catch blocks so errors in API stop crashing script

--- scripts/music.js
+++ scripts/music.js
@@ -3,15 +3,27 @@ const fs = require('fs');
 const {getTopAlbums, getTopArtists, getDiscogsCollectionItems} = require('./utils/music');
 
 (async () => {
-	const topArtistResponse = await getTopArtists(process.env.LAST_FM_API_KEY);
+	try {
+		const topArtistResponse = await getTopArtists(process.env.LAST_FM_API_KEY);
 
-	fs.writeFileSync('./src/data/artists.json', JSON.stringify(topArtistResponse.data.topartists.artist.slice(0, 8), null, '\t'));
+		fs.writeFileSync('./src/data/artists.json', JSON.stringify(topArtistResponse.data.topartists.artist.slice(0, 8), null, '\t'));
+	} catch (e) {
+		console.error('Top artist fetching failed', e);
+	}
 
-	const topAlbumResponse = await getTopAlbums(process.env.LAST_FM_API_KEY);
+	try {
+		const topAlbumResponse = await getTopAlbums(process.env.LAST_FM_API_KEY);
 
-	fs.writeFileSync('./src/data/albums.json', JSON.stringify(topAlbumResponse.data.topalbums.album.slice(0, 12), null, '\t'));
+		fs.writeFileSync('./src/data/albums.json', JSON.stringify(topAlbumResponse.data.topalbums.album.slice(0, 12), null, '\t'));
+	} catch (e) {
+		console.error('Top album fetching failed', e);
+	}
 
-	const discogsCollectionResponse = await getDiscogsCollectionItems(process.env.DISCOGS_TOKEN);
+	try {
+		const discogsCollectionResponse = await getDiscogsCollectionItems(process.env.DISCOGS_TOKEN);
 
-	fs.writeFileSync('./src/data/crate.json', JSON.stringify(discogsCollectionResponse, null, '\t'));
+		fs.writeFileSync('./src/data/crate.json', JSON.stringify(discogsCollectionResponse, null, '\t'));
+	} catch (e) {
+		console.error('crate data fetching failed', e);
+	}
 })();
