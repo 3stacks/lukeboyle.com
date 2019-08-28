c5e49cd634a70737494d5d892f93e8a60dbc7f4f
Luke Boyle <me@lukeboyle.com>
2019-08-28 22:28:25 +1000

    chore: wip on new post

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,33 +1,34 @@
+const colors = require('./src/styled/colors');
 require('dotenv').config();
 
 module.exports = {
-  siteMetadata: {
-    title: 'Luke Boyle | Front End Developer',
-    lastFMApiKey: process.env.LAST_FM_API_KEY,
-  	discogsApiKey: process.env.DISCOGS_TOKEN,
-  },
-  plugins: [
-    'gatsby-plugin-styled-components',
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-offline',
-    {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-            name: `images`,
-            path: `${__dirname}/src/assets/img`,
-        },
-    },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-    {
-        resolve: `gatsby-plugin-manifest`,
-        options: {
-            start_url: '/',
-            background_color: '#34495e',
-            theme_color: '#34495e',
-            display: 'minimal-ui',
-            icon: 'src/assets/img/android-icon-192x192.png', // This path is relative to the root of the site.
-        },
-    },
-  ]
+	siteMetadata: {
+		title: 'Luke Boyle | Front End Developer',
+		lastFMApiKey: process.env.LAST_FM_API_KEY,
+		discogsApiKey: process.env.DISCOGS_TOKEN
+	},
+	plugins: [
+		'gatsby-plugin-styled-components',
+		'gatsby-plugin-react-helmet',
+		'gatsby-plugin-offline',
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `images`,
+				path: `${__dirname}/src/assets/img`
+			}
+		},
+		'gatsby-transformer-sharp',
+		'gatsby-plugin-sharp',
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				start_url: '/',
+				background_color: colors.PRIMARY,
+				theme_color: colors.PRIMARY,
+				display: 'minimal-ui',
+				icon: 'src/assets/img/android-icon-192x192.png' // This path is relative to the root of the site.
+			}
+		}
+	]
 };
