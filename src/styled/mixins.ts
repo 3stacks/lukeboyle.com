53d984e9f06b9a40c5e674dfd444ff042b3b146b
Luke Boyle <me@lukeboyle.com>
2020-02-05 22:59:10 +1100

    chore: add blackshift mixin

--- src/styled/mixins.ts
+++ src/styled/mixins.ts
@@ -1,7 +1,26 @@
+import { css } from 'styled-components';
+import COLORS from './colors';
+
 export function bp(size, content) {
 	return `
 		@media (min-width: ${size}px) {
 			${content}
 		};
+	`;
+}
+
+export function blackShift(distance: number): string {
+	return css`
+		&:hover {
+			background-color: ${COLORS.WHITE};
+			color: ${COLORS.SECONDARY};
+			border: 2px solid ${COLORS.SECONDARY};
+
+			@media screen and (prefers-reduced-motion: no-preference) {
+				transform: translate(-${distance}px, -${distance}px);
+				box-shadow: ${distance + 2}px ${distance + 2}px 0 0
+					${COLORS.SECONDARY};
+			}
+		}
 	`;
 }
