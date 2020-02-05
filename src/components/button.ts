87deaa7f6f94ce0f0b02b8ad02e93038d3f286d0
Luke Boyle <me@lukeboyle.com>
2020-02-05 23:01:39 +1100

    chore: support reduced motion on buttons

--- src/components/button.ts
+++ src/components/button.ts
@@ -1,41 +1,42 @@
 import { Link } from 'gatsby';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import COLORS from '../styled/colors';
 
 function getButtonStyles({ isSecondary }) {
-	return `
+	return css`
 		display: inline-flex;
 		align-items: center;
-		${
-			isSecondary
-				? `
-			color: ${COLORS.SECONDARY};
-			background-color: #fff;
-			border: 2px solid ${COLORS.SECONDARY};
-		`
-				: `
-			color: white;
-			background-color: ${COLORS.SECONDARY};
-			border: none;
-		`
-		}
+		${isSecondary
+			? css`
+					color: ${COLORS.SECONDARY};
+					background-color: #fff;
+					border: 2px solid ${COLORS.SECONDARY};
+			  `
+			: css`
+					color: white;
+					background-color: ${COLORS.SECONDARY};
+					border: none;
+			  `}
+
 		padding: 10px 15px;
 		text-decoration: none;
-		transition: transform 0.2s ease-out, box-shadow 0.3s ease-out;
 		text-transform: uppercase;
 		font-size: 1.3rem;
 		font-weight: bold;
 		border: 2px solid ${COLORS.SECONDARY};
-		
+
 		&:hover,
 		&:focus {
 			color: ${COLORS.SECONDARY} !important;
 			outline: none;
-			transform: translate(-3px,-3px);
-        	background-color: #fff;
-        	border: 2px solid ${COLORS.SECONDARY};
-        	box-shadow: 4px 4px 0 0 ${COLORS.SECONDARY};
+			background-color: #fff;
+			border: 2px solid ${COLORS.SECONDARY};
 			color: ${COLORS.SECONDARY};
+
+			@media screen and (prefers-reduced-motion: no-preference) {
+				transform: translate(-3px, -3px);
+				box-shadow: 4px 4px 0 0 ${COLORS.SECONDARY};
+			}
 		}
 	`;
 }
