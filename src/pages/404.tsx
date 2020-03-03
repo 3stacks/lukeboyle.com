4e120d35b4a29525651e7baf0b2bcdbe05f48646
Luke Boyle <me@lukeboyle.com>
2020-03-03 23:48:55 +1100

    chore: remove blog header for 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import MaxWidthContainer from '../components/MaxWidthContainer';
 import Layout from '../components/Layout';
-import BlogHeader from '../components/BlogHeader/BlogHeader';
 import { LinkButton } from '../components/Button';
+import HomeHeadBanner from '../components/HomeHeadBanner/HomeHeadBanner';
 
 export default function NotFoundError() {
     return (
@@ -11,9 +11,9 @@ export default function NotFoundError() {
             <Helmet>
                 <title>Not Found | Luke Boyle</title>
             </Helmet>
-            <BlogHeader>
+            <HomeHeadBanner hasColor={false}>
                 <h1 className="site-name">Not found</h1>
-            </BlogHeader>
+            </HomeHeadBanner>
             <MaxWidthContainer style={{ textAlign: 'center' }}>
                 <p>
                     The page you are looking for doesn&apos;t exist. I&apos;ll
