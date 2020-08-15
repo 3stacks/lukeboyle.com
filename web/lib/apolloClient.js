83bd905217b3d4c7b06ffe5a75e9ccb52036b740
Luke Boyle <me@lukeboyle.com>
2020-08-15 11:24:42 +1000

    chore: add prettier precommit hook

--- web/lib/apolloClient.js
+++ web/lib/apolloClient.js
@@ -8,8 +8,8 @@ function createApolloClient() {
 	return new ApolloClient({
 		ssrMode: typeof window === 'undefined',
 		link: new HttpLink({
-			uri: 'http://localhost:4000', // Server URL (must be absolute)
-			credentials: 'same-origin' // Additional fetch() options like `credentials` or `headers`
+			uri: 'http://localhost:4000',
+			credentials: 'same-origin'
 		}),
 		cache: new InMemoryCache({
 			typePolicies: {
@@ -44,6 +44,5 @@ export function initializeApollo(initialState = null) {
 }
 
 export function useApollo(initialState) {
-	const store = useMemo(() => initializeApollo(initialState), [initialState]);
-	return store;
+	return useMemo(() => initializeApollo(initialState), [initialState]);
 }
