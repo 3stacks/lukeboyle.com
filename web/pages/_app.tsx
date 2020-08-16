73f0a991dc339b7e2cf3bb089362f0a0deded438
Luke Boyle <me@lukeboyle.com>
2020-08-16 14:09:21 +1000

    chore: move app icons to root and add favicon

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -6,51 +6,57 @@ import Layout from '../components/Layout/Layout';
 import { GlobalLayoutStyle } from '../styled/layout.style';
 
 export const MyApp = ({ Component, pageProps }) => {
-    const router = useRouter();
+	const router = useRouter();
 
-    return (
-        <Layout slug={router.asPath}>
-            <Head>
-                <title>{MY_NAME}</title>
-                <meta name="referrer" content="origin" />
-                <meta charSet="utf-8" />
-                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-                <link
-                    rel="apple-touch-icon"
-                    sizes="76x76"
-                    href="/img/apple-icon-76x76.png"
-                />
-                <link
-                    rel="apple-touch-icon"
-                    sizes="120x120"
-                    href="/img/apple-icon-120x120.png"
-                />
-                <link
-                    rel="apple-touch-icon"
-                    sizes="152x152"
-                    href="/img/apple-icon-152x152.png"
-                />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    sizes="192x192"
-                    href="/img/android-icon-192x192.png"
-                />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    sizes="32x32"
-                    href="/img/favicon-32x32.png"
-                />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1.0"
-                />
-            </Head>
-            <GlobalLayoutStyle />
-            <Component {...pageProps} />
-        </Layout>
-    );
+	return (
+		<Layout slug={router.asPath}>
+			<Head>
+				<title>{MY_NAME}</title>
+				<meta name="referrer" content="origin" />
+				<meta charSet="utf-8" />
+				<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+				<link
+					rel="apple-touch-icon"
+					sizes="76x76"
+					href="/apple-icon-76x76.png"
+				/>
+				<link
+					rel="apple-touch-icon"
+					sizes="120x120"
+					href="/apple-icon-120x120.png"
+				/>
+				<link
+					rel="apple-touch-icon"
+					sizes="152x152"
+					href="/apple-icon-152x152.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="192x192"
+					href="/android-icon-192x192.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon-32x32.png"
+				/>
+				<link
+					rel="shortcut icon"
+					href="/favicon.ico"
+					type="image/x-icon"
+				/>
+				<link rel="icon" href="/favicon.ico" type="image/x-icon" />
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1.0"
+				/>
+			</Head>
+			<GlobalLayoutStyle />
+			<Component {...pageProps} />
+		</Layout>
+	);
 };
 
 export default MyApp;
