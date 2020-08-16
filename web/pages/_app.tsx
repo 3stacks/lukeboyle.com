043c35f1df55212f515884a5fc7e08a42bf87590
Luke Boyle <me@lukeboyle.com>
2020-08-16 14:23:52 +1000

    chore: add google site verification tag

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -52,6 +52,10 @@ export const MyApp = ({ Component, pageProps }) => {
 					name="viewport"
 					content="width=device-width, initial-scale=1.0"
 				/>
+				<meta
+					name="google-site-verification"
+					content="JKQQdLNK9rQUZnixIsfEuJALcEcfPp9_ee2grLgOVGM"
+				/>
 			</Head>
 			<GlobalLayoutStyle />
 			<Component {...pageProps} />
