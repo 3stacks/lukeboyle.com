f235d01b44756e02974164ba76122d8e38170fca
Luke Boyle <me@lukeboyle.com>
2020-08-31 06:40:58 +1000

    chore: remove unused mixins

--- web/styled/mixins.ts
+++ web/styled/mixins.ts
@@ -1,14 +1,5 @@
-import styled, { css, FlattenSimpleInterpolation } from 'styled-components';
+import { css, FlattenSimpleInterpolation } from 'styled-components';
 import { CUSTOM_PROPERTIES } from './colors';
-
-export const ScreenReaderText = styled.span`
-	position: absolute !important;
-	clip: rect(1px, 1px, 1px, 1px);
-`;
-
-export const NoWrap = styled.span`
-	white-space: nowrap;
-`;
 
 export function bp(
 	size: number,
